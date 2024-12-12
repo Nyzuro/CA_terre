4a2526d65dddb4c613ebf074a5078baa3ccbe88c
Nyzuro <marco.strs27@gmail.com>
2024-12-12 23:21:03 +0100

    terre01.js

--- epreuves/terre/terre00.js
+++ epreuves/terre/terre00.js
@@ -1,7 +1,7 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-let result = "";
+let result = ""; //result servira a afficher le code sur une ligne seulement
 
-for (let i = 0; alphabet[i]; i++) {
-    result = result + alphabet[i];
+for (let i = 0; alphabet[i]; i++) { //i sert a se deplacer dans la string alphabet en changeant d'index tant que alphabet n'est pas finit(/0)
+    result = result + alphabet[i]; // on ajoute la lettre de l'index a result pour faire qu'une seule string
 }
 console.log(result);
---
+++ epreuves/terre/terre01.js
@@ -0,0 +1,3 @@
+const path = require('path');
+const filename = path.basename(__filename);
+console.log(filename);
