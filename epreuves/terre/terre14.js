c4ae0fc823f17a177eb3ad7972c5cc9c57ecbb49
Nyzuro <marco.strs27@gmail.com>
2024-12-20 14:30:44 +0100

    fix

--- epreuves/terre/terre14.js
+++ epreuves/terre/terre14.js
@@ -1,13 +1,32 @@
-const otherArguments = process.argv.slice(3)
-const firstArgument = process.argv.slice(2, 3)
+let otherArguments = process.argv.slice(3)
+let firstArgument = process.argv.slice(2, 3)
 
+firstArgument[0] = Number(firstArgument[0]) // Pour passer les arguments en chiffre
 for (const i in otherArguments) {
+    otherArguments[i] = Number(otherArguments[i])
+}
+
+if (!Number.isInteger(firstArgument[0])) { // Verifie si c'est un integer
+    console.log("erreur.")
+    return
+}
+
+if (!otherArguments[0]) { // pas assez d'arguments
+    console.log("Entrez au moins deux arguments")
+    return
+}
+
+for (const i of otherArguments) {
+    if (!Number.isInteger(otherArguments[i])) { // Verifie si c'est un integer
+        console.log("erreur.")
+        return
+    }
     if (firstArgument > otherArguments[i]) {
         console.log("Pas triee !")
         return
     }
     else {
-        console.log("Triee")
+        console.log("Triee !")
         return
     }
 }
