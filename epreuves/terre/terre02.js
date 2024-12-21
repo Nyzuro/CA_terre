11fc73089bdc794718b0c837262c747d816dd30c
Nyzuro <marco.strs27@gmail.com>
2024-12-21 16:15:33 +0100

    fix terre02

--- epreuves/terre/terre02.js
+++ epreuves/terre/terre02.js
@@ -1,6 +1,5 @@
-let i = 0;
+const arguments = process.argv.slice(2)
 
-while (process.argv.slice(2)[i]) { // on ne prend pas les deux premiers arguments
-    console.log(process.argv.slice(2)[i]);
-    i++;
+for (const i of arguments) {
+    console.log(i)
 }
