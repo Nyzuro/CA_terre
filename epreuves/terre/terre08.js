45de51d695c17e88b87c8b56880a00bd526f6904
Nyzuro <marco.strs27@gmail.com>
2024-12-22 16:48:30 +0100

    retry terre08

--- epreuves/terre/terre08.js
+++ epreuves/terre/terre08.js
@@ -1,11 +1,23 @@
-const process = require('process')
-const argument1 = process.argv[2]
-const argument2 = process.argv[3]
+const arguments = process.argv.slice(2)
+const firstArgument = Number(arguments[0])
+const secondArgument = Number(arguments[1])
+let power = firstArgument
 
-if (isNaN(argument1) == true || isNaN(argument2) == true || argument2 < 0) {
-    console.log('error.')
-}
-else {
-    const result = argument1 ** argument2;
-    console.log(result)
+if (isNaN(firstArgument) || isNaN(secondArgument)) {
+    console.error("Please enter only numbers")
+    process.exit()
+} if (arguments.length !== 2) {
+    console.error("Please enter 2 numbers")
+    process.exit()
+} if (secondArgument < 0) {
+    console.error("Exposant can't be negative")
+    process.exit()
+} if (secondArgument === 0) {
+    console.log(1)
+    process.exit()
+} else {
+    for (i = 1; i < secondArgument; i++) {
+        power = power * firstArgument
+    }
+    console.log(power)
 }
