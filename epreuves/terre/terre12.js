46023d24db26b92fcdf4acb4bf34cb6e25b9f52b
Nyzuro <marco.strs27@gmail.com>
2024-12-23 16:51:50 +0100

    fix terre12

--- epreuves/terre/terre12.js
+++ epreuves/terre/terre12.js
@@ -1,36 +1,39 @@
-const argument = process.argv.slice(2)
-if (!argument[0]) {
-    console.log("Rentrez un argument")
-    return
-}
-let hours = argument[0].slice(0, 2)
-let minutes = argument[0].slice(3, 5)
-let amOrpm = argument[0].slice(5, 7)
+const arguments = process.argv.slice(2)
 
-if (!/^\d{2}:\d{2}(AM|PM)$/.test(argument[0])) { //^\d{2} => deux chiffres pour les heures; :\d{2} => deux chiffres pour les minutes apres un ":"; (AM|PM) => Obligatoirement AM ou PM à la fin.
-    console.log("Ecrivez comme ceci 11:40PM");
-    return;
+if (arguments.length !== 1) {
+    console.error("Entrez 1 argument")
+    process.exit()
 }
 
-if (hours > 12) {
-    console.log("Une journee ne peut avoir que 24h")
-    return;
+let hours = Number(arguments[0].slice(0, 2))
+let minutes = Number(arguments[0].slice(3, 5))
+let amOrpm = arguments[0].slice(5, 7)
+
+if (isNaN(hours) || isNaN(minutes)) {
+    console.error("Entrez des chiffres")
+    process.exit()
 }
-if (minutes > 59) {
-    console.log("Il ne peut pas y avoir plus de 59 minutes")
-    return
+
+if (!/^(1[0-2]|0[0-9]):([0-5][0-9])(AM|PM|am|pm)$/.test(arguments[0])) {
+    console.error("Ecrivez comme ceci 00:00AM")
+    process.exit()
 }
-if (hours == 12 && amOrpm === "PM") {
-    console.log("00h s'ecrit avec AM")
-    return
-}
-if (hours == 12) {
-    hours = "00"
-}
-if (amOrpm === "PM") {
+
+if (hours === 12 && amOrpm === "AM") {
+    console.error("12AM n'existe pas")
+    process.exit()
+} if (hours === 0 && amOrpm === "PM") {
+    console.error("00PM n'existe pas")
+    process.exit()
+} if (hours === 12) {
+    console.log(`${hours}:${minutes.toString().padStart(2, "0")}`)
+    process.exit()
+} if (hours === 0) {
+    console.log(`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`);
+    process.exit();
+} if (amOrpm === "PM") {
     hours = hours + 12
-    console.log(`${hours}:${minutes}`)
-}
-else {
-    console.log(`${hours}:${minutes}`)
+    console.log(`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`)
+} else {
+    console.log(`${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`)
 }
